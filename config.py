d7823deb7f42fe1ecce95c557f58ca75f313fa0c
Zechariah Gaiya <abbazechariah@gmail.com>
2024-06-09 22:56:25 +0100

    Initial commit to heroku

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Admin17017!@localhost/github_use'
+    SQLALCHEMY_DATABASE_URI = os.getenv('JAWSDB_URL', 'mysql://root:Admin17017!@localhost/github_use')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
